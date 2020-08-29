3099f251c71164ba15faf3e336d5f47c220943aa
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-29 17:56:03 -0300

    Estilizando página de login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,48 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, FormEvent } from "react";
+import { Link } from "react-router-dom";
 
-import logo from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
-import background from '../../assets/images/background.svg';
-import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
-import './style.css';
-import '../../assets/styles/global.css';
+import showPassword from "../../assets/images/icons/show-password.svg";
+import hidePassword from "../../assets/images/icons/hide-password.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import "./style.css";
+import "../../assets/styles/global.css";
 
-import api from '../../services/api';
+import ProffyPage from '../../components/ProffyPage';
+import Cadastro from '../Cadastro';
+
+import api from "../../services/api";
 
 const Login = () => {
 
-  return (
-        <div id="landing-page">
-            <div className="proffy" style={{backgroundImage: `url(${background})`}}>
-                <img src={logo} className="logo"/>
-                <p className="title">Sua plataforma de estudos online.</p>
-            </div>
-            <div className="login">
-                <h1>Fazer login</h1>
-                <form action="">
-                    <fieldset>
-                        <input type="email" placeholder="E-mail"/>
-                        <input type="password" placeholder="Senha"/>
-                        <input type="checkbox"/>Lembrar-me
-                        <a href="#">Esqueci minha senha</a>
-                        <button type="submit">Entrar</button>
-                    </fieldset>
-                </form>
-                <div className="footer">
-                    <div className="create-account">
-                        <p>Não tem uma conta?</p>
-                        <a href="#">Cadastre-se</a>
-                    </div>
-                    <p>É de graça</p>
-                    <img src={purpleHeartIcon}/>
+    const [hiddenPassword, setHiddenPassword] = useState(true);
+    
+    function changePasswordActive() {
+        if(hiddenPassword) setHiddenPassword(false);
+        else setHiddenPassword(true);
+    }
 
-                </div>
+  return (
+    <div id="landing-page">
+      <ProffyPage/>
+
+      <div className="login">
+        <div className="login-form">
+          <h1>Fazer login</h1>
+          <form action="">
+            <fieldset>
+              <input type="email" placeholder="E-mail" />
+              <div>
+                {hiddenPassword ? 
+                    <img src={showPassword} alt="" onClick={changePasswordActive}/> 
+                    :  <img src={hidePassword} alt="" className="hidePass" onClick={changePasswordActive}/>}
+                
+                {hiddenPassword ? <input type="password" placeholder="Senha"/> : <input type="text" placeholder="Senha"/>}
+              </div>
+            </fieldset>
+            <section className="form-footer">
+              <div>
+                <label htmlFor="rememberMe">Lembrar-me
+                    <input type="checkbox" id="rememberMe"/>
+                    <span className="checkmark"></span>
+                </label>
+              </div>
+              <Link to="/forgot-password">
+                  Esqueci minha senha
+              </Link>
+            </section>
+            <button type="submit">Entrar</button>
+          </form>
+          <div className="footer">
+            <div>
+              <p>Não tem uma conta?</p>
+              <Link to="/sign-up">
+                  Cadastre-se
+              </Link>
             </div>
+            <div>
+              <p>É de graça</p>
+              <img src={purpleHeartIcon} />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
